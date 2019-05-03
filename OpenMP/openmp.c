27e6a6baa105ef7af7fe911d7ee0733d997deb6a
Reed-Anderson <reedineagan@comcast.net>
2019-05-02 21:09:47 -0500

    progress on openmp.c

--- OpenMP/openmp.c
+++ OpenMP/openmp.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <omp.h>
+
+int number_of_threads;
+int number_of_lines;
+char** longest_common;
+char** stored_file;
+
+void lcs()
+{
+  
+}
+
+void readFile()
+{
+
+}
 
 int main (int argc, char *argv[]) 
 {
@@ -12,6 +28,34 @@ int main (int argc, char *argv[])
   }
   else
   {
-    printf("The doctor will see you now");
+    // Collect Args
+    number_of_threads = atoi(argv[1]);
+    number_of_lines = atoi(argv[2]);
+    // OpenMP Variables
+    int start_point;
+    int end_point;
+    int thread_num;
+    char** temp_longest_common;
+    int i;
+    // OpenMP Parallelization
+    omp_set_num_threads(number_of_threads);
+    #pragma omp parallel private(myID, startPos, endPos, i, longPointer)
+    {
+      thread_num = omp_get_thread_num();
+      start_point = thread_num * number_of_lines / number_of_threads;
+      end_point = start_point + number_of_lines / number_of_threads;
+      temp_longest_common = longest_common + start_point;
+      
+      if (thread_num == number_of_threads - 1)
+      {
+          end_point = number_of_lines - 1;
+      }
+      
+      for (i = start_point; i < end_point; i++)
+      {
+          lcs((void*) stored_file[i], (void*)stored_file[i + 1], longest_common);
+          temp_longest_common++;
+      }
+    }
   }
 }
