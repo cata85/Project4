19ccc26e67a2e91989049358d4dc86cbac4f9d4b
Carter Nelson <cartern@ksu.edu>
2019-05-02 20:39:52 -0500

    ACTUAL base, no pragam omp stuff being used.

--- Base/base.c
+++ Base/base.c
@@ -12,6 +12,7 @@
 //this method is found https://gist.github.com/adrian-source/4111719 .... THIS CODE IS NOT MINE
 char* lcs(char* s1, char* s2)
 {
+  /*
   #pragma omp private(s1, s2, s1_l, s2_l, i, sl_i, s2_i,longest, set, v)
 
   int s1_l = strlen(s1), s2_l = strlen(s2);
@@ -70,6 +71,64 @@ char* lcs(char* s1, char* s2)
   
   // return
   return set;
+  */
+
+  int s1_l = strlen(s1), s2_l = strlen(s2);
+	int i, s1_i, s2_i, longest = 0;
+
+	char* set = (char*)malloc(sizeof(char) * ((s1_l < s2_l)?s1_l:s2_l));
+	memset(set, ' ', strlen(set));
+
+	// initialize 2d array
+	int** l = (int**)malloc((s1_l + 1) * sizeof(int*));
+	for (i = 0; i < s1_l + 1; i++)
+		l[i] = (int*)malloc((s2_l + 1) * sizeof(int));
+
+	// initialize all array fields to 0
+	for (s1_i = 0; s1_i < s1_l; s1_i++)
+		for (s2_i = 0; s2_i < s2_l; s2_i++)
+			l[s1_i][s2_i] = 0;
+
+	// loop through all letters
+	for (s1_i = 0; s1_i < s1_l; s1_i++)
+		for (s2_i = 0; s2_i < s2_l; s2_i++)
+		{
+			// if characters match
+			if (s1[s1_i] == s2[s2_i])
+			{
+				// calculate the incremented length for current sequence
+				int v = l[s1_i][s2_i] + 1;
+
+				// save the incremented length in south-east cell
+				l[s1_i+1][s2_i+1] = v;
+
+				// if new length is longest thus far
+				if (v > longest)
+				{
+					// set that to be our new longest length
+					longest = v;
+
+					// and clear the set
+					memset(set, ' ', strlen(set));
+				}	
+
+				// if new length equals to the longest length
+				if (v == longest)
+				{
+					// copy the char set to the return set
+					for (i = 0; i < longest; i++)
+						set[i] = s1[s1_i-longest+i+1];
+				}
+			}
+		}
+
+	// free
+	for (i = 0; i < s1_l + 1; i++)
+		free(l[i]);
+	free(l);
+
+	// return
+	return set;
 }
 
 // this was my implementation for LCS, I coudn't get it to work so I am using someone else's in order to get to the parrellism part of the assignment and start the write up on time
