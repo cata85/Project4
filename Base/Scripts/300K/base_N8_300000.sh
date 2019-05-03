b2a4da9fa804334791235ea03383b5cf4c48c671
Carter Nelson <cartern@ksu.edu>
2019-05-03 01:19:25 -0500

    Added all the scripts for base.

---
+++ Base/Scripts/300K/base_N8_300000.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+##### These lines are for Slurm
+#SBATCH -t 10:00:00                #Maximum time required
+#SBATCH -o Output/base_N8_300000.%j              #Output file name
+#SBATCH --mem-per-cpu=16G
+#SBATCH --constraint=elves
+
+### Job commands start here
+### Display some diagnostic information
+echo '=====================JOB DIAGNOTICS========================'
+date
+echo -n 'This machine is ';hostname
+echo -n 'My jobid is '; echo $SLURM_JOBID
+echo 'My path is:' echo $PATH
+echo 'My job info:'
+squeue -j $SLURM_JOBID
+echo 'Machine info'
+sinfo -s
+
+echo '=====================JOB STARTING=========================='
+
+{ time srun --nodes=1 --ntasks=8 base /homes/dan/625/wiki_dump.txt 300000 ; } 2> Times/base_N8_300000.txt
+
+echo '========================ALL DONE==========================='
--- Base/run.sh
+++ Base/run.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
 
 
+# Setup
+gcc -o base base.c
+
 # Nodes: 1
-sbatch Scripts/base_N1_10000.sh
-sbatch Scripts/base_N1_100000.sh
-sbatch Scripts/base_N1_300000.sh
-sbatch Scripts/base_N1_500000.sh
-sbatch Scripts/base_N1_1000000.sh
+sbatch Scripts/10K/base_N1_10000.sh
+sbatch Scripts/100K/base_N1_100000.sh
+sbatch Scripts/300K/base_N1_300000.sh
+sbatch Scripts/500K/base_N1_500000.sh
+sbatch Scripts/1000K/base_N1_1000000.sh
+
+# Nodes: 2
+sbatch Scripts/10K/base_N2_10000.sh
+sbatch Scripts/100K/base_N2_100000.sh
+sbatch Scripts/300K/base_N2_300000.sh
+sbatch Scripts/500K/base_N2_500000.sh
+sbatch Scripts/1000K/base_N2_1000000.sh
+
+# Nodes: 4
+sbatch Scripts/10K/base_N4_10000.sh
+sbatch Scripts/100K/base_N4_100000.sh
+sbatch Scripts/300K/base_N4_300000.sh
+sbatch Scripts/500K/base_N4_500000.sh
+sbatch Scripts/1000K/base_N4_1000000.sh
+
+# Nodes: 8
+sbatch Scripts/10K/base_N8_10000.sh
+sbatch Scripts/100K/base_N8_100000.sh
+sbatch Scripts/300K/base_N8_300000.sh
+sbatch Scripts/500K/base_N8_500000.sh
+sbatch Scripts/1000K/base_N8_1000000.sh
+
+# Nodes: 16
+sbatch Scripts/10K/base_N16_10000.sh
+sbatch Scripts/100K/base_N16_100000.sh
+sbatch Scripts/300K/base_N16_300000.sh
+sbatch Scripts/500K/base_N16_500000.sh
+sbatch Scripts/1000K/base_N16_1000000.sh
