e88643722afdea44656ed65eb5c61304a6f1c06d
Carlos Aguirre <caguirre97@ksu.edu>
2019-05-05 02:31:34 -0500

    mpi scripts

---
+++ MPI/Scripts/4core/mpi_N8_1000000.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+##### These lines are for Slurm
+#SBATCH -t 10:00:00                #Maximum time required
+#SBATCH -o Output/4core/mpi_N8_1000000.%j              #Output file name
+#SBATCH --mem-per-cpu=4G
+#SBATCH --constraint=elves
+#SBATCH --nodes=8
+#SBATCH --cpus-per-task=4
+
+### Job commands start here
+### Display some diagnostic information
+echo '=====================JOB DIAGNOTICS========================'
+date
+echo -n 'This machine is ';hostname
+echo -n 'My jobid is '; echo $SLURM_JOBID
+echo 'My path is:' echo $PATH
+echo 'My job info:'
+squeue -j $SLURM_JOBID
+echo 'Machine info'
+sinfo -s
+
+echo '=====================JOB STARTING=========================='
+
+{ time srun mpi /homes/dan/625/wiki_dump.txt 1000000; } 2> Times/4core/mpi_N8_1000000.txt
+
+echo '========================ALL DONE==========================='
